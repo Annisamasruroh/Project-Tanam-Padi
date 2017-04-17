b5c94b8051210cfab11efb22afcf57b70d59eda5
Annisamasruroh <annisamasruroh65@gmail.com>
2017-04-17 18:57:42 +0700

    Update home.php

--- home.php
+++ home.php
@@ -6,6 +6,28 @@
 	<h2><b>Aplikasi Prediksi Masa Tanam Padi</b></h2>
  
   </div> 
+<body>
+<div class="result">
 
+<div class="search">
+
+<input class="city_names" id="city_names" name="city_names" placeholder="Masukkan nama kota"/>
+
+<input type = "button" name="submit" value="Cari" id="submit" class="submit"/>
+
+</div>
+
+<div class="main">
+
+<span class="city"></span>
+
+<span class="country"></span>
+
+<div class="temp"></div>
+
+<div class="message"></div>
+
+</div>
+
+</div>
 </body>
-</html>
